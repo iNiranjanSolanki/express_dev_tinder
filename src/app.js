8682540fba48752c17f453cbb56e4c3828c2cf71
iniranjansolanki <iniranjansolanki@gmail.com>
2025-12-16 18:23:29 +0530

    Add Feat : Connect DB

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
-const port = 3000;      
-app.use("/test",(req,res)=>(res.send("Hello World!")));
+const {connectDB} =require("./config/db");
+const User = require('./models/user');
+const port = 7000;      
+app.post("/signup", async (req, res) => {
+        const userObj = {
+                username: "onlyVishesh",
+                firstName: "Vishesh",
+                email: "okVishesh360@gmail.com",
+                password: "vishesh1234",
+                avatar: "https://avatars.githubusercontent.com/u/121187728?v=4",
+                role: "admin",
+        };
 
-app.listen(port ,()=>(
-        console.log(`Server is running on http://localhost:${port}`
-)))
+        // creating a new instance of User Model
+        const user = new User(userObj);
+        try {
+                await user.save();
+                res.send("User Added successfully");
+        } catch (err) {
+                res.status(400).send("error saving the user" + err.message);
+        }
+});
+connectDB()
+        .then(() => {
+                console.log("database connection establish");
+                app.listen(port, () => (
+                        console.log(`Server is running on http://localhost:${port}`
+                        )))
+        })
+        .catch((err) => console.error(err));
+
 
